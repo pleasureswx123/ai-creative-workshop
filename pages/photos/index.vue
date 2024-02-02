1f791b29755bfa4b2a55f1e379b3531c5b5c8a13
shangwenxue <pleasureswx123@163.com>
2024-02-02 14:49:58 +0800

    feat: photo

---
+++ pages/photos/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <view class="page-container">
+    <QmNavTop></QmNavTop>
+    <view>123</view>
+  </view>
+</template>
+
+<script>
+
+</script>
+
+<style lang="scss" scoped>
+.page-container {
+  background: var(--bg-color1);
+  padding-bottom: calc(10rpx + constant(safe-area-inset-bottom));
+  padding-bottom: calc(10rpx + env(safe-area-inset-bottom));
+  font-size: 24rpx;
+}
+</style>
