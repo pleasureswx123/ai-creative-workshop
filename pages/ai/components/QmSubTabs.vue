f474bdfa74cb0ce1163c375f694da052d537b064
shangwenxue <pleasureswx123@163.com>
2024-02-16 17:43:30 +0800

    feat: 免算力

--- pages/ai/components/QmSubTabs.vue
+++ pages/ai/components/QmSubTabs.vue
@@ -1,13 +1,25 @@
 <template>
   <view class="tabs-container" v-if="list.length">
-    <view class="tabs-box">
+<!--    <view class="tabs-box">
       <u-tabs :current="currentValue" :list="list" @change="handleChange"
               lineWidth="30rpx"
               lineHeight="8rpx"
               :activeStyle="{ color: '#303133', fontSize: '28rpx', fontWeight: 'bold' }"
               :inactiveStyle="{ color: '#606266', fontSize: '28rpx' }"
       ></u-tabs>
-    </view>
+    </view>-->
+    <scroll-view class="tabs-inner-box" scroll-x="true">
+      <view
+          v-for="(item, index) in list"
+          :key="`${item.type}-${index}`"
+          :data-id="item.type"
+          class="item" :class="{active: currentValue === index}"
+          @tap="currentValue = index"
+      >
+        <text>{{item.name}}</text>
+        <view class="flag" :class="{show: ['cjyy3'].includes(item.type)}">免算力</view>
+      </view>
+    </scroll-view>
   </view>
 </template>
 
@@ -58,6 +70,40 @@ export default {
   border-bottom: 1rpx solid #efefef;
   display: flex;
   align-items: center;
+  .tabs-inner-box {
+    white-space: nowrap;
+    width: 100%;
+    .item {
+      display: inline-block;
+      padding: 0 20px;
+      height: 70rpx;
+      line-height: 70rpx;
+      color: #606266;
+      font-size: 24rpx;
+      position: relative;
+      cursor: pointer;
+      &.active {
+        font-size: 28rpx;
+        color: #303133;
+        font-weight: bold;
+      }
+      .flag {
+        display: none;
+        position: absolute;
+        top: 4rpx;
+        right: 0;
+        background: #F60652;
+        font-size: 16rpx;
+        line-height: 16rpx;
+        padding: 4rpx 8rpx;
+        color: #fff;
+        border-radius: 20rpx;
+        &.show {
+          display: block;
+        }
+      }
+    }
+  }
   .tabs-box {
     width: 100%;
   }
