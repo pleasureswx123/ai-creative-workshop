3e81896b3e2c79c7a105104135c264c6e73fb639
shangwenxue <pleasureswx123@163.com>
2024-02-29 10:35:41 +0800

    feat: 重构

--- pages/picture/video-tool.vue
+++ pages/picture/video-tool.vue
@@ -7,9 +7,13 @@
       <view class="grid-box">
         <view class="card-box pointer" v-for="(item, index) in videoToolsList" :data-type="item.type" :key="item.id" @tap="handleClick(item)">
           <view class="img-box">
-            <image :src="item.url"></image>
+            <image model="aspectFill" :src="item.url"></image>
           </view>
-          <view class="title">{{item.title}}</view>
+          <view class="card-info">
+            <view class="title">{{item.title}}</view>
+            <view class="tips" v-if="item.tips">{{item.tips}}</view>
+            <view class="go-btn">前往创作</view>
+          </view>
         </view>
       </view>
     </view>
@@ -53,41 +57,71 @@ export default {
 }
 .grid-box {
   display: grid;
-  grid-template-columns: repeat(2, 1fr);
-  gap: 40rpx;
+  grid-template-columns: repeat(1, 1fr);
+  gap: 20rpx;
   margin-top: 30rpx;
   .card-box {
+    cursor: pointer;
     min-width: 0;
     border-radius: 20rpx;
-    background-color: var(--bg-color2);
     overflow: hidden;
     box-shadow: 0 0 20rpx rgba(0,0,0,.1);
+    padding: 10rpx;
+    background: #25262B;
+    box-sizing: border-box;
+    border: 2rpx solid #373A40;
+    display: flex;
+    align-items: center;
+    gap: 20rpx;
     .title {
       font-size: 24rpx;
       color: var(--txt-color1);
       text-align: center;
-      line-height: 80rpx;
+      line-height: 70rpx;
+    }
+    .tips {
+      font-size: 20rpx;
+      color: var(--txt-color1);
+      text-align: center;
+      line-height: 1.5;
+      padding: 0 0 15rpx;
     }
   }
+  .card-info {
+    flex: 1;
+    min-width: 0;
+    padding-right: 10rpx;
+  }
   .img-box {
-    aspect-ratio: 4 / 3;
+    width: 374rpx;
+    height: 280rpx;
     image {
       display: block;
       width: 100%;
       height: 100%;
     }
   }
+  .go-btn {
+    height: 70rpx;
+    line-height: 70rpx;
+    text-align: center;
+    color: #fff;
+    border-radius: 10rpx;
+    background: var(--red-color1);
+    font-weight: bold;
+    font-size: 28rpx;
+  }
 }
 
 @media screen and (min-width: 750px) and (max-width: 960px){
   .grid-box {
-    grid-template-columns: repeat(3, 1fr);
+    grid-template-columns: repeat(2, 1fr);
   }
 }
 
 @media screen and (min-width: 960px) {
   .grid-box {
-    grid-template-columns: repeat(4, 1fr);
+    grid-template-columns: repeat(3, 1fr);
   }
 }
 </style>
