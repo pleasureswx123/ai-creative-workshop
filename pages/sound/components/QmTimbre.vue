91fd7328af7575a6b2d31ca1157a89a922459aee
wanganqi <1249611476@qq.com>
2024-03-14 19:53:42 +0800

    声音克隆

---
+++ pages/sound/components/QmTimbre.vue
@@ -0,0 +1,63 @@
+<template>
+	<view class="box-container">
+		<view class="navTitle">
+			<view v-for="(item,index) in tabList" :key="index" class="item" :class="{'active':isActive == index}" @tap="checked(index)">
+				{{item.name}}
+			</view>
+		</view>
+		<view class="nav_item" v-if="isActive==0">
+			<Timbre></Timbre>
+		</view>
+		<view class="nav_item" v-if="isActive==1">
+			
+		</view>
+	</view>
+</template>
+
+<script>
+import Timbre from './Timbre.vue';
+export default{
+	components: {Timbre},
+	data() {
+		return{
+			tabList:[{
+				name:'音色'
+			},{
+				name:'配置'
+			}],
+			isActive:0
+		}
+	},
+	methods:{
+		checked(index){
+			this.isActive = index
+		}
+	}
+}
+</script>
+
+<style lang="scss" scoped>
+.box-container{
+	background: #25262b;
+	border: 1px solid #373a40;
+	padding: 5px 10px 10px;
+	border-radius: 4px;
+	margin: 20rpx 0;
+	color: #9E9E9E;
+}
+.navTitle{
+	display: flex;
+	font-size: 32rpx;
+	margin: 20rpx 0 ;
+	.item{
+		margin-right: 30rpx;
+		cursor: pointer;
+	}
+	.active {
+		border-bottom: 1px solid #9E9E9E;
+	}
+}
+.nav_item{
+	padding: 10rpx 0 0 20rpx;
+}
+</style>
---
+++ pages/sound/home.vue
@@ -0,0 +1,43 @@
+<template>
+  <page-meta page-style="background: var(--bg-color1)" />
+  <view class="page-container">
+    <QmNavTop></QmNavTop>
+    <PicHeader title="欢迎使用文字转语音功能，你的智能配音助手！" />
+    <TitleCell title="配音文案">
+      <template #right>
+        <text @tap="jumpHistory">历史配音</text>
+      </template>
+    </TitleCell>
+    <QmTextarea :value.sync.trim="describe" :maxlength="300"></QmTextarea>
+	<QmTimbre></QmTimbre>
+  </view>
+</template>
+
+<script>
+import PicHeader from './components/PicHeader.vue';
+import TitleCell from './components/TitleCell.vue';
+import QmTextarea from './components/QmTextarea.vue';
+import QmTimbre from './components/QmTimbre.vue';
+
+export default {
+  components: { PicHeader, TitleCell, QmTextarea, QmTimbre },
+  data() {
+    return {
+      describe: ''
+    }
+  },
+  methods: {
+	  jumpHistory() {
+	    uni.$u.route({url: 'pages/sound/history'})
+	  },
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.page-container {
+  background: var(--bg-color1);
+  font-size: 24rpx;
+  padding: 0 30rpx 300rpx;
+}
+</style>
