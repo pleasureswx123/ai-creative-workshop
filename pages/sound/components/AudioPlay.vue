46611c3ba12ae374e26a8b6c7ef9a08288ca9927
wanganqi <1249611476@qq.com>
2024-03-15 19:59:53 +0800

    数字人

--- pages/sound/components/AudioPlay.vue
+++ pages/sound/components/AudioPlay.vue
@@ -2,48 +2,112 @@
 	<view>
 		<view class="text">梦里寻梦，携手同行。<br>心之所向，未来可期。<br>共赴长路，不畏风雨。<br>笑对人生，岁月静好。</view>
 		<view class="tips">请用普通话大声朗读上方文字</view>
-		<view class="audio" v-if="playShow">
-		  <view class="play-icon" @tap="start"><i class="iconfont icon-luzhi1"></i></view>
-		  <view class="play-name">{{playtext}}</view>
-		</view>
-		<view class="audio" v-else>
-			<view class="prompt">
+		<view class="audio">
+			<view v-if='recorder' class="overplay">
+				<view>红色字读错了，建议重录</view>
+				<view class="try-listen" @tap="play">
+				  <text>试听</text>
+				  <uni-icons custom-prefix="iconfont-qm" type="icon-qm-volume" color="var(--txt-color1)" size="20" />
+				</view>
+			</view>
+			<view class="prompt" v-if="promptShow">
 				<view class="prompt-loader">
 					<view class="em" v-for="(item,index) in 15" :key="index"></view>
 				</view>
 			</view>
-			<view class="play-icon" @tap="start"><i class="iconfont icon-tingzhiluzhi"></i></view>
-			<view class="play-name">{{playtext}}</view>
+			<view>
+				<view @click='handlerOnCahnger' class="play-icon" v-if="playShow">
+					<i class="iconfont icon-luzhi1"></i><view>{{statusName}}</view>
+				</view>
+				<view @click='handlerStopCahnger' class="play-icon" v-else>
+					<i class="iconfont icon-tingzhiluzhi"></i><view>{{statusName}}</view>
+				</view>
+				<view v-if="ReprodShow" class="play-reprod">
+					<i class="iconfont icon-luzhi1 icon-luzhi2"></i><view>复刻</view>
+				</view>
+			</view>
+			<mumu-recorder ref='recorder' @success='handlerSuccess' @error='handlerError'></mumu-recorder>
 		</view>
 	</view>
 </template>
 <script>
-const recorderManager = uni.getRecorderManager();
-const innerAudioContext = uni.createInnerAudioContext();
-innerAudioContext.autoplay = true;
 export default{
 	data(){
 		return{
-			playtext:'点击录制',
-			playShow:true,
-			voicePath: ''
+			statusName:'点击录制',
+			playShow:'true',
+			recorder: false,
+			audioContext: null,
+			promptShow:false,
+			ReprodShow:false
 		}
 	},
 	onLoad() {
-		let self = this;
-		recorderManager.onStop(function (res) {
-			console.log('recorder stop' + JSON.stringify(res));
-			self.voicePath = res.tempFilePath;
-		});
+		
+	},
+	beforeDestroy() {
+	  this.destroyAudio();
 	},
 	methods:{
-		start(){
-			if(this.playtext){
-				this.playtext = '停止录制'
-				this.playShow = false
+		handlerSave() {
+			let tag = document.createElement('a')
+			tag.href = this.recorder.localUrl
+			tag.download = '录音'
+			tag.click()
+		},
+		handlerOnCahnger() {
+			this.$refs.recorder.start()
+			this.playShow = false
+			this.promptShow = true
+			this.recorder = false
+			this.statusName ='停止录制'
+			this.ReprodShow = false
+		},
+		handlerStopCahnger(){
+			this.$refs.recorder.stop()
+			this.playShow = true
+			this.promptShow = false
+			this.recorder = true
+			this.statusName ='重新录制'
+			this.ReprodShow = true
+		},
+		handlerSuccess(res) {
+			this.recorder = res
+			this.recorder.localUrl = res.localUrl
+		},
+		handlerError(code) {
+			switch (code) {
+				case '101':
+					uni.showModal({
+						content: '当前浏览器版本较低，请更换浏览器使用，推荐在微信中打开。'
+					})
+					break;
+				case '201':
+					uni.showModal({
+						content: '麦克风权限被拒绝，请刷新页面后授权麦克风权限。'
+					})
+					break
+				default:
+					uni.showModal({
+						content: '未知错误，请刷新页面重试'
+					})
+				break
 			}
-			console.log('开始录音');
-			recorderManager.start();
+		},
+		destroyAudio() {
+		  if(this.audioContext) {
+		    this.audioContext.pause();
+		    this.audioContext.destroy();
+		    this.audioContext = null;
+		  }
+		},
+		play() {
+		  this.destroyAudio();
+		  this.audioContext = uni.createInnerAudioContext();
+		  this.audioContext.src = this.recorder.localUrl;
+		  // 设置音量（范围：0 到 1）
+		  this.audioContext.volume = 0.5;
+		  this.audioContext.play();
 		}
 	}
 }
@@ -149,5 +213,23 @@ export default{
 }
 .prompt-loader .em:nth-child(15) {
  animation: load 2.5s 1.4s infinite linear;
+}
+.overplay,.try-listen{
+	display: flex;
+	align-items: center;
+	justify-content: center;
+}
+.try-listen{
+	margin-left: 100rpx;
+}
+.overplay{
+	margin-bottom: 60rpx;
+}
+.icon-luzhi2:before{
+	color: #73B06F!important;
+}
+.play-reprod,.play-icon{
+	display: inline-block;
+	margin:0 20rpx;
 }
 </style>
---
+++ pages/sound/components/AudioUpload.vue
@@ -0,0 +1,53 @@
+<template>
+	<view class="upload-box-container pointer" @tap="handleUpload">
+	  <view class="upload-box">
+	    <view class="icon-box">
+	      <i class="iconfont icon-shangchuan_huaban"></i>
+	    </view>
+	    <view class="title">点击上传音频</view>
+	  </view>
+	</view>
+</template>
+
+<script>
+	export default{
+		data(){
+			return{
+				
+			}
+		},
+		methods:{
+			handleUpload(){
+				var that = this
+				uni.chooseFile({
+					count: 1,   //选取的文件个数限制
+					extension: [".mp3"],  //可定义允许哪些后缀的文件可被选择
+					success: function (res) {
+					  let tempFilePaths = res.tempFilePaths;
+					  uni.showLoading({
+						title: "上传中...",
+					  });
+					  
+					  uni.uploadFile({
+						name: 'audio',    //文件上传的name值
+						url: 'https://localhost:8099/web.php/upload/audio',    //接口地址
+						header:{},    //头信息
+						formData:{},   //上传额外携带的参数
+						filePath: tempFilePaths[0],//临时路径
+						fileType: "audio",   //文件类型
+						success: (uploadFileRes) => {
+						  uni.hideLoading();
+						  const ret = JSON.parse(uploadFileRes.data);
+						  console.log(ret)
+						},
+					  });
+					  
+					},
+				});
+			},
+		}
+	}
+</script>
+
+<style>
+</style>
